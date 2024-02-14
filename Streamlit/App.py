362d1ea61bbf1bab3b41b6401cfa6e2dacc04129
mayank3484 <142235171+mayank3484@users.noreply.github.com>
2024-02-14 12:41:42 +0530

    Update App.py

--- Streamlit/App.py
+++ Streamlit/App.py
@@ -10,7 +10,7 @@ def main():
     
     st.markdown(html_temp,unsafe_allow_html=True)
     
-    model= joblib.load('LinearRegression.pkl')
+    model= joblib.load('Streamlit/LinearRegression.pkl')
     p1= st.number_input('Enter Your Age in range 18 to 100',18,100,"min",1,format="%d")
     s1= st.selectbox('Sex',('Male','Female'))
     
